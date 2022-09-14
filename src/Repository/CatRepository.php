44e7bcfc3fdb32e86fbc192ac2c4636c96e15f98
Norio Suzuki <norio.suzuki@gmail.com>
2022-09-15 08:02:24 +0900

    Add new CatRepository

---
+++ src/Repository/CatRepository.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Repository;
+
+use App\Model\v000\Cat as Cat000;
+use App\Model\v101\Cat as Cat101;
+use App\Model\v102\Cat as Cat102;
+use App\Model\v103\Cat as Cat103;
+use App\Model\v104\Cat as Cat104;
+use App\Model\v105\Cat as Cat105;
+use App\Model\v200\Cat as Cat200;
+use App\Model\v300\Cat as Cat300;
+use App\Model\v400\Cat as Cat400;
+use App\Model\v500\Cat as Cat500;
+
+final class CatRepository
+{
+    private array $cats = [];
+
+    public function __construct(
+        private string $className,
+    ) {
+        $this->initialize();
+    }
+
+    public function find(int $id): Cat000|Cat101|Cat102|Cat103|Cat104|Cat105|Cat200|Cat300|Cat400|Cat500|null
+    {
+        return $this->cats[$id] ?? null;
+    }
+
+    /**
+     * @return \Generator<Cat000|Cat101|Cat102|Cat103|Cat104|Cat105|Cat200|Cat300|Cat400|Cat500>
+     */
+    public function iterateAll(): \Generator
+    {
+        foreach ($this->cats as $cat) {
+            yield $cat;
+        }
+    }
+
+    private function initialize(): void
+    {
+        $data = [
+            ['id' => 1, 'name' => 'Mugi', 'imageUrl' => 'mugi.jpg', 'gender' => 'Female', 'weight' => 1.8],
+            ['id' => 2, 'name' => 'Sora', 'imageUrl' => 'sora.jpg', 'gender' => 'Male',   'weight' => 2.4],
+            ['id' => 3, 'name' => 'Leo',  'imageUrl' => 'leo.jpg',  'gender' => 'Male',   'weight' => 2.2],
+            ['id' => 4, 'name' => 'Coco', 'imageUrl' => 'coco.jpg', 'gender' => 'Female', 'weight' => 3.1],
+        ];
+
+        foreach ($data as $d) {
+            $cat = (new $this->className())
+                ->setId($d['id'])
+                ->setName($d['name'])
+                ->setImageUrl($d['imageUrl'])
+                ->setGender($d['gender'])
+                ->setWeight($d['weight']);
+
+            $this->cats[$cat->getId()] = $cat;
+        }
+    }
+}
---
+++ tests/Repository/CatRepositoryTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Repository;
+
+use App\Model\v000\Cat as Cat000;
+use App\Model\v101\Cat as Cat101;
+use App\Model\v102\Cat as Cat102;
+use App\Model\v103\Cat as Cat103;
+use App\Model\v104\Cat as Cat104;
+use App\Model\v105\Cat as Cat105;
+use App\Model\v200\Cat as Cat200;
+use App\Model\v300\Cat as Cat300;
+use App\Model\v400\Cat as Cat400;
+use App\Model\v500\Cat as Cat500;
+use PHPUnit\Framework\TestCase;
+
+final class CatRepositoryTest extends TestCase
+{
+    /**
+     * @dataProvider catModelProvider
+     */
+    public function testFind(string $model_class): void
+    {
+        $repo = new CatRepository($model_class);
+
+        $cat = $repo->find(1);
+        $this->assertSame(1, $cat->getId());
+        $this->assertSame('Mugi', $cat->getName());
+        $this->assertSame('mugi.jpg', $cat->getImageUrl());
+        $this->assertSame('Female', $cat->getGender());
+        $this->assertSame(1.8, $cat->getWeight());
+
+        $unknown_cat = $repo->find(0);
+        $this->assertNull($unknown_cat);
+    }
+
+    public function testIterateAll(): void
+    {
+        $repo = new CatRepository(Cat000::class);
+
+        $idList = [];
+        foreach ($repo->iterateAll() as $cat) {
+            $idList[] = $cat->getId();
+        }
+        sort($idList);
+
+        $this->assertSame([1, 2, 3, 4], $idList);
+    }
+
+    public function catModelProvider(): array
+    {
+        return [
+            [Cat000::class],
+            [Cat101::class],
+            [Cat102::class],
+            [Cat103::class],
+            [Cat104::class],
+            [Cat105::class],
+            [Cat200::class],
+            [Cat300::class],
+            [Cat400::class],
+            [Cat500::class],
+        ];
+    }
+}
